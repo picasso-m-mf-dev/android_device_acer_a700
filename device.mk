3e80253e24db40bb76321eef14c01a13ac1431cf
elibl <elischa.blaess@gmail.com>
2014-03-19 17:45:18 +0100

    use mdpi
    Change-Id: Icc27297547c7f5cfe5f1cf7a857a973dc64b5007

--- device.mk
+++ device.mk
@@ -1,5 +1,8 @@
 $(call inherit-product-if-exists, vendor/acer/a510/a510-vendor.mk)
 
+PRODUCT_AAPT_CONFIG := normal large xlarge mdpi hdpi
+PRODUCT_AAPT_PREF_CONFIG := xlarge mdpi
+
 # USB config contains product-specific USB id
 PRODUCT_COPY_FILES += \
     device/acer/a510/prebuilt/ramdisk/init.acer.usb.rc:root/init.acer.usb.rc \
