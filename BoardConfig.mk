d5dc330e726bcc684aa9b03bcfe2be175713beff
elibl <elischa.blaess@gmail.com>
2014-06-11 16:47:21 +0200

    initial lvm
    Change-Id: I082f55c93f2a6c7ae6879fe5678db5428b162fab

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,6 +1,9 @@
 # inherit from the proprietary version
 -include vendor/acer/a510/BoardConfigVendor.mk
 
+# Inherit from t30-common
+include device/acer/t30-common/BoardConfigCommon.mk
+
 TARGET_BOOTLOADER_BOARD_NAME := picasso_m
 TARGET_OTA_ASSERT_DEVICE := picasso_m,a510_pa_cus1
 
@@ -10,5 +13,5 @@ TARGET_KERNEL_CONFIG := cyanogenmod_picasso_m_defconfig
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/acer/a510/bluetooth
 
-# Inherit from t30-common
-include device/acer/t30-common/BoardConfigCommon.mk
+# Recovery
+TARGET_RECOVERY_FSTAB := device/acer/a510/prebuilt/ramdisk/fstab.acer
--- device.mk
+++ device.mk
@@ -5,13 +5,25 @@ PRODUCT_AAPT_PREF_CONFIG := xlarge mdpi
 
 # USB config contains product-specific USB id
 PRODUCT_COPY_FILES += \
-    device/acer/a510/prebuilt/ramdisk/init.acer.usb.rc:root/init.acer.usb.rc \
+    device/acer/a510/prebuilt/ramdisk/init.acer.usb.rc:root/init.acer.usb.rc
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=160
 
 # So init files are copied properly in device_base.mk
 PRODUCT_BOOTLOADER := picasso_m
+
+PRODUCT_COPY_FILES += \
+    device/acer/a510/prebuilt/ramdisk/init.picasso_m.rc:root/init.picasso_m.rc \
+    device/acer/a510/prebuilt/ramdisk/init.recovery.picasso_m.rc:root/init.recovery.picasso_m.rc \
+    device/acer/a510/prebuilt/ramdisk/fstab.acer:root/fstab.acer \
+    device/acer/a510/prebuilt/ramdisk/fstab.acer:root/fstab.picasso_m
+
+# lvm files
+PRODUCT_COPY_FILES += \
+    device/acer/a510/prebuilt/ramdisk/lvm/etc/lvm.conf:root/lvm/etc/lvm.conf \
+    device/acer/a510/prebuilt/ramdisk/lvm/sbin/lvm:root/lvm/sbin/lvm \
+    device/acer/a510/prebuilt/ramdisk/lvm/sbin/setuplvmsystem.sh:recovery/root/lvm/sbin/setuplvmsystem.sh
 
 # Inherit t30-common
 $(call inherit-product, device/acer/t30-common/device_base.mk)
